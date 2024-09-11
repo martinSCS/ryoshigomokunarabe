e1f0c042cee38de013087ae34a8dafb5c71026a0
MartinGrocery <53111703+martinSCS@users.noreply.github.com>
2024-09-11 10:54:38 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,6 +2,14 @@ import GoBoard from "@/components/goblock";
 
 export default function Home() {
     return (
+        <html>
+        <head>
+            <title>量子五目並べ</title>
+            <meta charSet={'utf-8'}/>
+        </head>
+        <body>
         <GoBoard />
+        </body>
+        </html>
     );
 }
